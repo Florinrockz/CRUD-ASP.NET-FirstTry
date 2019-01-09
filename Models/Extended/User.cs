cafb7659376836aeeb1352884cb361229083b28a
Scutaru Florin - Adrian <33432925+Florinrockz@users.noreply.github.com>
2019-01-10 01:31:37 +0200

    Add files via upload

---
+++ Models/Extended/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_ASP.Models
+{
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User
+    {
+
+    }
+
+    public class UserMetaData
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        public string name
+        {
+            get;
+            set;
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        public string email
+        {
+            get;
+            set;
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        public string phone
+        {
+            get;
+            set;
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        public string country
+        {
+            get;
+            set;
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        public string city
+        {
+            get;
+            set;
+        }
+    }
+}
